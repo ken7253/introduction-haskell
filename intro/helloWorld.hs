c3510f873bc5e1bcc9b1591a29d70a71bbd7bb96
ken7253 <ken7253@gmail.com>
2025-06-15 16:55:45 +0900

    いろいろ追加

---
+++ intro/helloWorld.hs
@@ -0,0 +1,6 @@
+main = foo
+
+foo = do
+  putStrLn "hello, what's your name?"
+  name <- getLine
+  putStrLn ("Hey" ++ name ++ ", you rock!")
--- intro/intro.hs
+++ intro/intro.hs
@@ -2,8 +2,9 @@ succ n = n + 1
 
 square x = x * x
 
-fact n | n == 0 = 1
-       | n > 0 = n * fact (n-1)
+fact n
+  | n == 0 = 1
+  | n > 0 = n * fact (n - 1)
 
 isEven n = n `mod` 2 == 0
 
@@ -11,4 +12,9 @@ isOdd n = n `mod` 2 /= 0
 
 isPositiveEven n = n > 0 && n `mod` 2 == 0
 
-isPrime n = n > 1 && all (\k -> n `mod` k /= 0) [2..n-1]
+isPrime n = n > 1 && all (\k -> n `mod` k /= 0) [2 .. n - 1]
+
+removeNonUpperCase :: [Char] -> String
+removeNonUpperCase str = [x | x <- str, x `elem` ['A' .. 'Z']]
+
+isTriples = [(a, b, c) | c <- [1 .. 10], a <- [1 .. c], b <- [1 .. a], a ^ 2 + b ^ 2 == c ^ 2, a + b + c == 24]
